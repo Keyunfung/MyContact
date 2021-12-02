fc7f2f6da1dbd6ce742a4b418aeb8e33ea771e19
Keyunfung <keyun2201@gmail.com>
2021-12-02 16:45:04 +0800

    Third commit

--- app/src/main/java/com/example/mycontact/model/Contact.kt
+++ app/src/main/java/com/example/mycontact/model/Contact.kt
@@ -1,4 +1,13 @@
 package com.example.mycontact.model
 
-data class Contact (var name: String = "", var phone: String = "") {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//Entity = Table (OOP = Class)
+@Entity(tableName = "contact")
+data class Contact (var name: String = "", @PrimaryKey var phone: String = "") {
+
+    override fun toString(): String {
+        return "$name : $phone"
+    }
 }
