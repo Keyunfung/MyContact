fa961ec78b13600fe2d61ba01ea5c87b27a423e9
Keyunfung <keyun2201@gmail.com>
2021-12-02 22:14:09 +0800

    Third commit

--- app/src/main/java/com/example/mycontact/database/ContactDatabase.kt
+++ app/src/main/java/com/example/mycontact/database/ContactDatabase.kt
@@ -5,11 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.mycontact.dao.ContactDao
-import com.example.mycontact.databinding.ContentMainBinding
 import com.example.mycontact.model.Contact
 
-@Database (entities = arrayOf(Contact::class), version = 1, exportScheme = false)
-abstract class ContactDatabase : RoomDatabase {
+@Database (entities = arrayOf(Contact::class), version = 1, exportSchema = false)
+abstract class ContactDatabase : RoomDatabase() {
     abstract fun contactDao(): ContactDao
 
     companion object {
